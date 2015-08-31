9dc4ed616d546d7b1650c25c4d2df5cd6d711a45
tillda <michal.till@gmail.com>
2015-08-31 15:09:20 +0200

    Example

--- example.js
+++ example.js
@@ -1,3 +1,4 @@
+<<<<<<< f82af63f2982a8f380b46d1ce58905fe31ba1af6
 var multiserve = require("multiserve");
 
 function startDevServer() {
@@ -9,6 +10,19 @@ function startDevServer() {
             { urlPath: '/',               fsPath: "bar" }
         ]
     }); 
+=======
+var multiserve = require("./multiserve");
+
+function startDevServer() {
+    multiserve.start({
+        port: 10555,
+        routing: [
+            {urlPath: '/', fsPath: "env/stackone/htmlclient/index-dev.html"},
+            {urlPath: '/style.css', fsPath: "src/vendor/htmlclient1/style.css"},
+            {urlPath: '/htmlclient1-dev-working-directory', fsPath: "target/htmlclient1-dev-working-directory/"},
+        ]
+    });
+>>>>>>> Example
 }
 
 startDevServer();
